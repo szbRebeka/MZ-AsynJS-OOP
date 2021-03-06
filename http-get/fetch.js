7feabfe1cef6ad8bfbc385b9f209f24cdbfb932d
szbRebeka <szrebeka94@gmail.com>
2021-03-06 11:06:51 +0100

    finish fetch exercise

--- http-get/fetch.js
+++ http-get/fetch.js
@@ -1,4 +1,42 @@
-//can be deleted
-console.log("Console log works!");
+/*
+api forrás: https://jsonplaceholder.typicode.com/users
+
+- A `fetch.js` fájlban írj egy GET kérést a fent megadott API címre!
+- használd lehetőleg a Fetch API-t!
+- Használj hibakezelést:
+    - Sikeres kérés esetén a visszakapott adatot irasd ki console-ra!
+    - Hiba esetén írasd ki a dobott hibát!
+ */
 
 
+const API_URL = 'https://jsonplaceholder.typicode.com/users';
+
+/*
+fetch(API_URL).then((response) => {
+   // console.log('resolve', response);
+    //getting the data
+    return response.json();
+
+}).then(data => {
+    console.log('resolved: ', data);
+}).catch((error) => {
+    console.log('rejected: ', error)
+});
+*/
+
+//fetch with async
+
+const getUsers = async () => {
+
+    const response = await fetch(API_URL);
+
+    if (response.status !== 200) {
+        throw new Error('cannot fetch the data');
+    }
+    const data = await response.json();
+    return data;
+}
+
+getUsers()
+    .then(data => console.log('resolved: ', data))
+    .catch(error => console.log('rejected: ', error.message));
